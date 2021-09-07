ef5def4fc07806fcfb642fa75ce9984fefb0dffb
Robert Kuzhin <robertkuzhin@gmail.com>
2021-09-07 18:32:02 +0500

    fix: change selector

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Application } from './app/application';
 
-ReactDOM.render(<Application />, document.querySelector('root'));
+ReactDOM.render(<Application />, document.querySelector('#root'));

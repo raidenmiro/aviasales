fba945a0d232625a50bca6f32a8498f3862af4ba
Robert Kuzhin <robertkuzhin@gmail.com>
2021-09-07 20:36:31 +0500

    feat: add default exports how components

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import reactSvgPlugin from 'vite-plugin-react-svg';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default defineConfig({
   base: '/aviasales/',
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    reactSvgPlugin({
+      defaultExport: 'component',
+      memo: true,
+    }),
+  ],
 });

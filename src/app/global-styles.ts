31d446803253cdba3c0c66e7cf3927bd0f49f9c1
Robert Kuzhin <robertkuzhin@gmail.com>
2021-09-07 18:32:24 +0500

    feat: add global styles

---
+++ src/app/global-styles.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  :root{
+    --beige: #F3F7FA;
+    --blue: #2196F3;
+    --black: #4A4A4A;
+    --gray: #A0B0B9;
+
+    --size-small: 1.2rem;
+    --size-medium: 1.4rem;
+    --size-large: 2rem;
+
+    --shadow-small: 0px 2px 8px rgba(0, 0, 0, 0.1);
+
+    --background: var(---gray);
+  }
+
+  html,
+  body,
+  #root {
+    height: 100%;
+  }
+
+  html{
+    font-size: 10px;
+  }
+
+  body{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;

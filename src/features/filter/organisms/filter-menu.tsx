60852d3a41cedae5658dc4718d472a811b242308
lordSzn <robertkuzhin@gmail.com>
2021-09-08 18:07:47 +0500

    refactor(filter): change on checkbox list

--- src/features/filter/organisms/filter-menu.tsx
+++ src/features/filter/organisms/filter-menu.tsx
@@ -1,42 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
-import { CheckBox } from '../../../ui/atoms';
+import { CheckBoxList } from '../molecules/checkbox-list';
 
 export const FilterMenu = () => {
   return (
     <FormGroup>
       <Typography>Количество пересадок</Typography>
-      <List>
-        <Li>
-          <CheckBox name="all" />
-        </Li>
-        <Li>
-          <CheckBox name="non" />
-        </Li>
-        <Li>
-          <CheckBox name="1tr" />
-        </Li>
-        <Li>
-          <CheckBox name="2tr" />
-        </Li>
-        <Li>
-          <CheckBox name="3tr" />
-        </Li>
-      </List>
+      <FormList>
+        <CheckBoxList />
+      </FormList>
     </FormGroup>
   );
 };
 
-const FormGroup = styled.form``;
+const FormGroup = styled.form`
+  padding: 2rem 1.5rem;
+  max-width: 23rem;
+  box-shadow: var(--shadow-small);
+  border-radius: 5px;
+`;
 
 const Typography = styled.h3`
-  font-size: var(--size-small);
+  font-size: var(--size-medium);
   color: var(--black);
   font-weight: var(--weight);
 
   margin-bottom: 2rem;
 `;
 
-const List = styled.ul``;
-
-const Li = styled.li``;
+const FormList = styled.ul`
+  margin: 0;
+  padding: 0;
+`;

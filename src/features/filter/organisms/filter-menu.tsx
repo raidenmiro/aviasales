7a782439d1d06a5f56b2b2c0372a192f15ea11be
lordSzn <robertkuzhin@gmail.com>
2021-09-08 16:36:49 +0500

    feat(filter): add filter form

--- src/features/filter/organisms/filter-menu.tsx
+++ src/features/filter/organisms/filter-menu.tsx
@@ -1,5 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+import { CheckBox } from '../../../ui/atoms';
 
 export const FilterMenu = () => {
-  return <div>menu</div>;
+  return (
+    <FormGroup>
+      <Typography>Количество пересадок</Typography>
+      <List>
+        <Li>
+          <CheckBox name="all" />
+        </Li>
+        <Li>
+          <CheckBox name="non" />
+        </Li>
+        <Li>
+          <CheckBox name="1tr" />
+        </Li>
+        <Li>
+          <CheckBox name="2tr" />
+        </Li>
+        <Li>
+          <CheckBox name="3tr" />
+        </Li>
+      </List>
+    </FormGroup>
+  );
 };
+
+const FormGroup = styled.form``;
+
+const Typography = styled.h3`
+  font-size: var(--size-small);
+  color: var(--black);
+  font-weight: var(--weight);
+
+  margin-bottom: 2rem;
+`;
+
+const List = styled.ul``;
+
+const Li = styled.li``;

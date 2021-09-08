cbf238b4354482ecf032fff4dc3c58ca69a69bc9
lordSzn <robertkuzhin@gmail.com>
2021-09-08 18:17:16 +0500

    fix: update styles

--- src/template/base.tsx
+++ src/template/base.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { FilterMenu } from '../features/filter';
 import { Header } from '../features/header/organisms/header';
 
+
 interface Props {
   header?: ReactNode;
   filter?: ReactNode;
@@ -28,7 +29,7 @@ const Container = styled.section`
   height: 100%;
   overflow: hidden;
   max-width: 97rem;
-  margin: 0 auto;
+
 
   grid-template-columns: minmax(auto, 35rem) 1fr;
   grid-template-rows: minmax(auto, 16rem) 1fr;

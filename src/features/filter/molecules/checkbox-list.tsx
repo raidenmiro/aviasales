f1aa0bea9a78318acec067babfe250b345d6267e
lordSzn <robertkuzhin@gmail.com>
2021-09-08 18:07:19 +0500

    feat(filter): add checkbox list

---
+++ src/features/filter/molecules/checkbox-list.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { useList } from 'effector-react';
+import { CheckBox } from '../../../ui/atoms';
+import { $filterItems } from '../model';
+import styled from 'styled-components';
+
+export const CheckBoxList = () =>
+  useList($filterItems, ({ title, active }, index) => (
+    <Group>
+      <CheckBox
+        name={title}
+        text={title}
+        checked={active}
+        onChange={console.log}
+      />
+    </Group>
+  ));
+
+const Group = styled.li`
+  &:not(:last-child) {
+    margin-bottom: 2rem;
+  }
+`;

3c451dcc2e8d2c0855d685685539fcac6d8441b7
lordSzn <robertkuzhin@gmail.com>
2021-09-09 17:56:27 +0500

    feat: add cell for ticket

---
+++ src/entities/ticket/atoms/cell.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  title: string;
+  content: string;
+}
+
+export const Cell = ({ title, content }: Props) => {
+  return (
+    <Container>
+      <Typography>{title}</Typography>
+      <Content>{content}</Content>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  font-weight: (--weight);
+`;
+
+const Typography = styled.h4`
+  color: var(--gray);
+  font-size: var(--size-small);
+
+  letter-spacing: 0.5px;
+  margin-bottom: 1rem;
+`;
+
+const Content = styled.div`
+  font-size: var(--size-medium);
+  color: var(--black);
+`;

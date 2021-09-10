842e9ba2c7e86a3f21c9d41586527a9f24e00ad5
lordSzn <robertkuzhin@gmail.com>
2021-09-10 21:15:03 +0500

    feat(app): start request for search id

--- src/app/application.tsx
+++ src/app/application.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { searchIdGetFx } from '../api/requests';
 
 import { Main } from '../pages';
 import { GlobalStyles } from './global-styles';
 
 export function Application() {
+  useEffect(() => {
+    searchIdGetFx();
+  }, []);
   return (
     <>
       <GlobalStyles />

7946d41af3d693269af0f70399d5ba61a4e9f0de
lordSzn <robertkuzhin@gmail.com>
2021-09-10 21:22:18 +0500

    feat: add plugin for ts check

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import reactSvgPlugin from 'vite-plugin-react-svg';
+import checker from 'vite-plugin-checker';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
       defaultExport: 'component',
       memo: true,
     }),
+    checker({ typescript: true }),
   ],
 });

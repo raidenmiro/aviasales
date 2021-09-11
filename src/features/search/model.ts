e4cf20301ab0f0b74335d9ddc0e2950768cc6769
lordSzn <robertkuzhin@gmail.com>
2021-09-11 14:13:05 +0500

    feat(search): add load tickets with searchId

--- src/features/search/model.ts
+++ src/features/search/model.ts
@@ -1,8 +1,13 @@
-import { createStore } from 'effector';
-import { loadSearchIdFx } from '../../api';
+import { createStore, forward } from 'effector';
+import { loadSearchIdFx, loadTicketsFx } from '../../api';
 import { SearchType } from '../../types/entities';
 
 export const $searchId = createStore<SearchType | null>(null).on(
   loadSearchIdFx.doneData,
   (_, searchId) => searchId
 );
+
+forward({
+  from: loadSearchIdFx.doneData,
+  to: loadTicketsFx,
+});

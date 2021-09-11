51dc9a3eb348e9097cb03c1e504e162215862420
lordSzn <robertkuzhin@gmail.com>
2021-09-11 13:56:23 +0500

    refactor(api): change name semantic

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -2,7 +2,7 @@ import ky from 'ky';
 import { createEffect } from 'effector';
 import { SearchType, Ticket } from '../types/entities';
 
-export const searchIdGetFx = createEffect<void, SearchType>({
+export const loadSearchIdFx = createEffect<void, SearchType>({
   handler: async () => {
     const searchId = await ky
       .get('https://front-test.beta.aviasales.ru/search')
@@ -12,7 +12,7 @@ export const searchIdGetFx = createEffect<void, SearchType>({
   },
 });
 
-export const ticketsGetFx = createEffect<SearchType, Ticket[]>({
+export const loadTicketsFx = createEffect<SearchType, Ticket[]>({
   handler: async ({ searchId }) => {
     const tickets = ky
       .get(`https://front-test.beta.aviasales.ru/tickets?searchId=${searchId}`)

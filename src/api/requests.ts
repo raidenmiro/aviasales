9f3badd0942b5512eed50708425fca9598a63d22
lordSzn <robertkuzhin@gmail.com>
2021-09-11 13:40:58 +0500

    fix(api): fix type and format

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,12 +1,13 @@
-import { createEffect } from 'effector';
 import ky from 'ky';
-import { Search } from '../types/entities';
+import { createEffect } from 'effector';
+import { SearchType } from '../types/entities';
 
-export const searchIdGetFx = createEffect<void, Search>({
+export const searchIdGetFx = createEffect<void, SearchType>({
   handler: async () => {
-    const searchId = await ky.get(
-      'https://front-test.beta.aviasales.ru/search'
-    );
-    return searchId.json();
+    const searchId = await ky
+      .get('https://front-test.beta.aviasales.ru/search')
+      .json<SearchType>();
+
+    return searchId;
   },
 });

79c280039e83a8fe3876bb48e692f6de2c8088a2
lordSzn <robertkuzhin@gmail.com>
2021-09-11 13:53:12 +0500

    feat: add type ticket

--- src/types/entities.ts
+++ src/types/entities.ts
@@ -3,3 +3,26 @@ export type Uuid = string;
 export interface SearchType {
   searchId: Uuid;
 }
+
+export interface Ticket {
+  price: number;
+  carrier: string;
+  segments: [
+    {
+      /*  there */
+      origin: string;
+      destination: string;
+      date: string;
+      stops: string[];
+      duration: number;
+    },
+    {
+      /* back */
+      origin: string;
+      destination: string;
+      date: string;
+      stops: string[];
+      duration: number;
+    }
+  ];
+}

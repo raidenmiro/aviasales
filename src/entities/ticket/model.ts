c210262943578d3044f713a3be88f9b7ec758568
lordSzn <robertkuzhin@gmail.com>
2021-09-12 17:31:56 +0500

    refactor: move normalize tickets in model

--- src/entities/ticket/model.ts
+++ src/entities/ticket/model.ts
@@ -1,6 +1,8 @@
+import { normalizeTickets } from './normalize';
 import { createEvent, createStore, guard, sample } from 'effector';
 import { loadTicketsFx } from '../../api';
 import { Ticket } from '../../types/entities';
+import { NormalizeTickets } from './types/ticket-normalize';
 
 export const saveTickets = createEvent<Ticket[]>();
 export const updateCacheTickets = createEvent<Ticket[]>();
@@ -26,3 +28,15 @@ guard({
   filter: Boolean,
   target: updateCacheTickets,
 });
+
+export const $normalizeTickets = createStore<NormalizeTickets[]>([]);
+
+sample({
+  source: $cacheTickets,
+  fn: (tickets) => tickets.map(normalizeTickets),
+  target: $normalizeTickets,
+});
+
+export const $visibleTickets = $normalizeTickets.map((tickets) =>
+  tickets.slice(0, 5)
+);

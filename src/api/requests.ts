f0eb0681e7a14ca575253b5c66bff9463f706afb
lordSzn <robertkuzhin@gmail.com>
2021-09-12 20:47:31 +0500

    fix: format

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -19,7 +19,9 @@ interface Config {
 
 export const loadTicketsFx = createEffect<SearchType, Config>({
   handler: async ({ searchId }) => {
-    const tickets = await fetch(`https://front-test.beta.aviasales.ru/tickets?searchId=${searchId}`)
+    const tickets = await fetch(
+      `https://front-test.beta.aviasales.ru/tickets?searchId=${searchId}`
+    );
     return tickets.json();
   },
 });

2ef8b75e22fcbf66c51f86eb6cd73ea10ada4235
lordSzn <robertkuzhin@gmail.com>
2021-09-12 13:35:28 +0500

    feat: replace ticket preview on content component

--- src/pages/main/page.tsx
+++ src/pages/main/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { TicketPreview } from '../../entities/ticket';
 import { BaseTemplate } from '../../template/base';
+import { Content } from './content';
 
 export const Main = () => {
   return (
-    <BaseTemplate>
-      <TicketPreview />
-    </BaseTemplate>
+    <>
+      <BaseTemplate>
+        <Content />
+      </BaseTemplate>
+    </>
   );
 };

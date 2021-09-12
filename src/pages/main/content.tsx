5180f0afef0a9d72f5a874b9930d70ed9890bf6b
lordSzn <robertkuzhin@gmail.com>
2021-09-12 14:34:12 +0500

    feat: add loading

--- src/pages/main/content.tsx
+++ src/pages/main/content.tsx
@@ -1,6 +1,7 @@
 import { useStore } from 'effector-react';
 import React from 'react';
-import { TicketPreview } from '../../entities/ticket';
+import styled from 'styled-components';
+import { TicketPreview, TicketsSkelleton } from '../../entities/ticket';
 import { $normalizeTickets } from '../../entities/ticket/normalize';
 import { $failMessage, $notStopSearch } from '../../features/search';
 
@@ -10,11 +11,25 @@ export const Content = () => {
   const normalizeTickets = useStore($normalizeTickets);
 
   if (loading && !fail) {
-    return <div>loading</div>;
+    return (
+      <>
+        {Array.from({ length: 3 }, (_, index) => (
+          <Container key={index}>
+            <TicketsSkelleton />
+          </Container>
+        ))}
+      </>
+    );
   }
+
   if (fail) {
     return <div>max requests</div>;
   }
 
   return <TicketPreview />;
 };
+
+const Container = styled.div`
+  margin-bottom: 1rem;
+  max-width: 54rem;
+`;

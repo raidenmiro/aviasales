323c0a0c7909080dc32105fe16da0a882ad19b15
lordSzn <robertkuzhin@gmail.com>
2021-09-12 17:37:22 +0500

    feat: add type props for cell

--- src/entities/ticket/ui/molecules/direction-row.tsx
+++ src/entities/ticket/ui/molecules/direction-row.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import { NormalizeSegment } from '../../types/ticket-normalize';
 import { Cell } from '../atoms';
 
-export const DirectionRow = () => {
+export const DirectionRow = ({
+  onTheWay,
+  transplants,
+  route,
+}: NormalizeSegment) => {
   return (
     <Container>
       <Cell title="hello" content="kek" />

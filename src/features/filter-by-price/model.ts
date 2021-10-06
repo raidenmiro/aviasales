d754159a1eac2dabd539c1a5f11c7ad76d537717
lordSzn <robertkuzhin@gmail.com>
2021-10-06 15:56:21 +0500

    feat: add filter by low price

--- src/features/filter-by-price/model.ts
+++ src/features/filter-by-price/model.ts
@@ -1,3 +1,29 @@
-import { createEvent } from 'effector';
+import { createEvent, createStore, sample } from 'effector';
+import { $normalizeTickets } from '../../entities/ticket';
 
-export const filterByPrice = createEvent();
+type Tabs = 'low' | 'fasted' | 'optimal';
+
+export const filterLowPrice = createEvent();
+export const filterByFastet = createEvent();
+export const filterByOptimal = createEvent();
+
+export const $activeTab = createStore<Tabs>('low')
+  .on(filterLowPrice, () => 'low')
+  .on(filterByFastet, () => 'fasted')
+  .on(filterByOptimal, () => 'optimal');
+
+sample({
+  clock: filterLowPrice,
+  source: $normalizeTickets,
+  fn: (tickets) => {
+    const sortByLowPrice = tickets.slice().sort((a, b) => {
+      const firstElement = a.price.value;
+      const secondElement = b.price.value;
+
+      return firstElement - secondElement;
+    });
+    return sortByLowPrice;
+  },
+  target: $normalizeTickets,
+});
+

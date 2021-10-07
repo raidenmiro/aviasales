4a9cc271a16d618751bdfc459663a59b94ed85f2
lordSzn <robertkuzhin@gmail.com>
2021-10-07 16:11:58 +0500

    test(entities): add test on of correct parse price

--- src/entities/ticket/lib/make-format.spec.ts
+++ src/entities/ticket/lib/make-format.spec.ts
@@ -1,1 +1,19 @@
 import { makePrice, makeTransfer, makeTransferTitle } from './make-format';
+
+describe('normalize tickets', () => {
+  describe('make price', () => {
+    it('check that the price is correct parse', () => {
+      const price = 13400;
+      const expected = `13\u00A0400 Р`;
+
+      expect(makePrice(price)).toBe(expected);
+    });
+
+    it('should be parse', () => {
+      const price = 0;
+      const expected = '0 Р';
+
+      expect(makePrice(price)).toBe(expected);
+    });
+  });
+});

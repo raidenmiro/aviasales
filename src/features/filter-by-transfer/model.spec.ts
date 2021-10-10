027018d37005c648669b5a9e7f2596429a0aa60f
lordSzn <robertkuzhin@gmail.com>
2021-10-10 18:22:56 +0500

    test(feature): add prototype tests

---
+++ src/features/filter-by-transfer/model.spec.ts
@@ -0,0 +1,16 @@
+describe('filters', () => {
+  describe('activation', () => {
+    it.todo('check active all filters');
+    it.todo('when clicking on the active filter, except "all" to disable it');
+    it.todo('when clicked on the active filter "all", disable others filters');
+    it.todo('if any filter is inactive, disabled filter "all"');
+    it.todo('if all filters are active except "all", then turn it on');
+    it.todo('if disable the filter "all", then everyone will be turned off');
+  });
+
+  describe('events filters', () => {
+    it.todo('filtering tickets by 1,2 transfers');
+    it.todo('filtering tickets by all');
+    it.todo('filtering without transplants');
+  });
+});

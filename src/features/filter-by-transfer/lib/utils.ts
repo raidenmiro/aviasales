83c554e7300f8ed979b626c3715ecf19cc5e3db9
lordSzn <robertkuzhin@gmail.com>
2021-10-11 19:22:27 +0500

    refactor(feature): delete unnecessary

--- src/features/filter-by-transfer/lib/utils.ts
+++ src/features/filter-by-transfer/lib/utils.ts
@@ -8,15 +8,3 @@ interface Config {
 export const isEveryActiveFilters = (filters: Config[]) => {
   return filters.slice(1).every((filter) => filter.active);
 };
-
-export const toggleSpecificFilter = (filters: Config[], type: string) => {
-  return filters.map((filter) =>
-    filter.id === type ? { ...filter, active: !filter.active } : filter
-  );
-};
-
-export const toggleAllFilter = (filters: Config[]) => {
-  const all = filters.slice(0, 1).map(({ active }) => active);
-
-  return filters.map((filter) => ({ ...filter, active: !all }));
-};
